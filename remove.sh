040749c0885bb806c9c2aa050e4a1dcd1bcbbe25
Deva <dvrharsha@gmail.com>
2024-10-25 15:49:52 -0400

    updated

--- remove.sh
+++ remove.sh
@@ -5,11 +5,11 @@ for envi in {prod,test,dev}; do
     if [ -d "$envi" ]; then
         echo "Processing environment: $envi"
 
-        # Find and delete all *.hcl files
-        find "$envi" -type f -name "*.lock.hcl" -exec rm -f {} \;
-        
         # Find and delete all *.terraform directories
         find "$envi" -type d -name ".terragrunt-cache" -exec rm -rf {} \;
+
+        # Find and delete all *.hcl files
+        find "$envi" -type f -name "*.lock.hcl" -exec rm -f {} \;
 
         echo "Deletion complete in $envi."
     else
--- terragrunt.sh
+++ terragrunt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd env/
 terragrunt run-all init -upgrade
 terragrunt run-all plan
 terragrunt run-all apply --terragrunt-non-interactive
